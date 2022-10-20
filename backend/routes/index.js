7d6c25243bc0f1f699bd2e94fc99f2f79daefe96
ChangeDL <douglas.loizzo@gmail.com>
2022-10-20 10:43:41 -0400

    Auth Me Added

---
+++ backend/routes/index.js
@@ -0,0 +1,18 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
+});
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+module.exports = router;
