e961e00b20e622466d67a10c04abb30215ab24eb
ChangeDL <douglas.loizzo@gmail.com>
2022-10-20 16:35:22 -0400

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,17 @@
+// backend/routes/api/index.js
 const router = require('express').Router();
-
-
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 router.get(
     '/restore-user',
     (req, res) => {
