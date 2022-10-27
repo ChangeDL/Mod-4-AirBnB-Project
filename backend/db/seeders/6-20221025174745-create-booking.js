1af34ae2e0578c38f99bb9df570324801b7545e0
ChangeDL <douglas.loizzo@gmail.com>
2022-10-26 23:11:55 -0400

    adjusted booking seeder

--- backend/db/seeders/6-20221025174745-create-booking.js
+++ backend/db/seeders/6-20221025174745-create-booking.js
@@ -20,25 +20,25 @@ module.exports = {
         endDate: '2025-10-25'
       },
       {
-        spotId: 1,
+        spotId: 2,
         userId: 2,
         startDate: '2025-11-24',
         endDate: '2025-11-25'
       },
       {
-        spotId: 2,
+        spotId: 3,
         userId: 2,
         startDate: '2025-10-24',
         endDate: '2025-10-25'
       },
       {
-        spotId: 2,
+        spotId: 4,
         userId: 3,
         startDate: '2025-11-24',
         endDate: '2025-11-25'
       },
       {
-        spotId: 2,
+        spotId: 5,
         userId: 3,
         startDate: '2025-9-24',
         endDate: '2025-9-25'
