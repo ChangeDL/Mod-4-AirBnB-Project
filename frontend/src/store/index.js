f5c0a5b94e9358a49cfb9b8568a338b1cbcd31d8
ChangeDL <douglas.loizzo@gmail.com>
2022-11-11 10:58:24 -0500

    Login setup for frontend with LoginFormPage and thunks

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,11 @@
 // frontend/src/store/index.js
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
-
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
+    session: sessionReducer,
 });
-
 
 let enhancer;
 
