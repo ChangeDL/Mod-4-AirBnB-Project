75b219f02bcb0b3da5e4541c00a871690e494cb6
ChangeDL <douglas.loizzo@gmail.com>
2022-11-11 16:22:00 -0500

    Modal added to frontend with login

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -15,7 +15,7 @@ function SignupFormPage() {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState([]);
 
-    if (Object.values(sessionUser).length > 0) return <Redirect to="/" />;
+    if (sessionUser) return <Redirect to="/" />;
 
     const handleSubmit = (e) => {
         e.preventDefault();
