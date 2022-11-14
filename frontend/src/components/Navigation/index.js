fc7d83d0d3089bc207724f81372b2f2e8b9d3fb0
ChangeDL <douglas.loizzo@gmail.com>
2022-11-14 11:29:24 -0500

    undid change because signup stopped working.

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -10,7 +10,7 @@ function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
 
     let sessionLinks;
-    if (sessionUser && Object.values(sessionUser).length > 0) {
+    if (sessionUser) {
         sessionLinks = (
             <ProfileButton user={sessionUser} />
         );
