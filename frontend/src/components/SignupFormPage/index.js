adcc1ece70c829673bc28e462968f05d333fe841
ChangeDL <douglas.loizzo@gmail.com>
2022-11-18 19:51:24 -0500

    bug fix for erros when signing up

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -24,7 +24,8 @@ function SignupFormPage() {
             return dispatch(sessionActions.signup({ firstName, lastName, email, username, password }))
                 .catch(async (res) => {
                     const data = await res.json();
-                    if (data && data.errors) setErrors(data.errors);
+
+                    if (data && data.message) setErrors([data.message]);
                 });
         }
         return setErrors(['Confirm Password field must be the same as the Password field']);
@@ -33,7 +34,7 @@ function SignupFormPage() {
     return (
         <form onSubmit={handleSubmit}>
             <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                {Object.values(errors).map((error, idx) => <li key={idx}>{error}</li>)}
             </ul>
             <label>
                 First Name
